8d92a1874aa35cb34179e6b4c54abb347bed185b
tfq <tfq55390@gmail.com>
2023-07-16 17:28:25 +0600

    Initial commit

---
+++ src/test/java/com/llv2/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llv2.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
